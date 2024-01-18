11aa749ca2774c7fa7dcd456d441deca073cf012
Vishal Maharathy <vishalmaharathy76@gmail.com>
2024-01-18 09:35:42 +0530

    google credentials to .env

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -7,7 +7,7 @@ const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
-    <GoogleOAuthProvider clientId="900023061203-498uqp6lkj8gup0o1lgbmiqn63romq1f.apps.googleusercontent.com">
+    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_OAUTH}>
         <App />
     </GoogleOAuthProvider>
 );
